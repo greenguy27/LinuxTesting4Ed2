e22766ce17814b5f9b422e49ce02bce754d67708
greenguy27 <cer5061@gmail.com>
2015-09-03 23:44:14 -0500

    Desktop
    created desltpop errorr=

--- KioskDeploy.sh
+++ KioskDeploy.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-##########################################################################
-# This is the main script that executes a menu to select applications to
-# install. After that, it starts the installation and configuration
-# process and finally it shows a log file which contains reported
-# installation steps and posible errors.
+
 #
-# Author: Chris Ross
-# Version: 1.0
-# Last modified date (dd/mm/yyyy): 9/3/2015
-# Licence: MIT
-##########################################################################
+# installer.sh: installs multiple ubuntu apps at once
+#
+# Copyright (C) 2015 Chris Ross
+#
+
+# make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# add repositories whatever you need
+sudo add-apt-repository -y ppa:webupd8team/java -y  #oracle-java
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -  #google-chrome-stable
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'  #google-chrome-stable
+notify-send --expire-time=3000 "All repositories are added successfully."
+
+# update the source list and upgrade the packages
+sudo apt-get -yq update
+sudo apt-get -yq upgrade
+notify-send --expire-time=3000 "Update & Upgrade" "Update & upgrade successfully completed."
 
 if [[ $(getconf LONG_BIT) = "64" ]]
 then
@@ -25,3 +37,39 @@ else
     sudo dpkg -i MistKiosk_i386.deb &&
     rm -f MistKiosk_i386.deb
 fi
+
+add package names to array below you want to install them
+apps=(
+   "aptitude"
+   "ubuntu-restricted-extras"
+   "oracle-java8-installer"
+   "oracle-java8-set-default"
+   "google-chrome-stable"
+   "vim"
+   "gparted"
+   "rar"
+   "samba"
+   "vlc"
+   "bijiben"
+   "tree"
+   "meld"
+   "libxml2-utils"
+   "winff"
+   "pepperflashplugin-nonfree"
+   "flashplugin-installer"
+)
+
+for app in "${apps[@]}"
+do
+   sudo apt-get -yq install $app
+   #notify-send --expire-time=3000 "$app" "Installed successfully."
+   echo ">>> >>> >>> $app installed."
+   sleep 2
+done
+
+appsToRemove=( tomboy hexchat thunderbird* simple-scan gthumb pidgin xchat* brasero* banshee transmission* )
+
+for app in "${appsToRemove[@]}"
+do
+   sudo apt-get purge -qy $app
+done
